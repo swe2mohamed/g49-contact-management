1572039ca1b674907aae37f83c3c1e7a81a9d1fd
swe2mohamed <swe.mohamed1990@gmail.com>
2024-03-18 13:48:48 +0100

    1.th version

--- src/main/java/se/lexcon/lc/App.java
+++ src/main/java/se/lexcon/lc/App.java
@@ -1,5 +1,7 @@
 package se.lexcon.lc;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(ContactDate.size());
+        boolean isAddSimonContact =  ContactDate.addContact("Simon,987654321,simon@mail.com");
+        System.out.println("isAddSimonContact = " + isAddSimonContact);
+        System.out.println(ContactDate.size());
+        System.out.println(Arrays.toString(ContactDate.getAllContacts()));
+        boolean isAddSimonContact2 =  ContactDate.addContact("Simon,987654321,simon@mail.com");
+        System.out.println("isAddSimonContact = " + isAddSimonContact2);
+
+
+
     }
 }
---
+++ src/main/java/se/lexcon/lc/ContactDate.java
@@ -0,0 +1,46 @@
+package se.lexcon.lc;
+
+import java.util.Arrays;
+
+public class ContactDate {
+    // Fields
+     private final static int MAX_SIZE = 100;
+    // static final field names are declared in uppercase words separated by underscores.
+
+      private static String[] contacts = new String[0]; // ["Name, MobileNumber, Email"]
+
+
+    public static boolean addContact(final String newContact) // "Name, MobileNumber, Email"
+    {
+        // step 1: validate the parameter & check the para exists or not
+        if (contacts.length < MAX_SIZE && !isDuplicateMobileNumber(newContact)){
+            // todo: need completion
+            String[] newArray = Arrays.copyOf(contacts,contacts.length + 1);
+            newArray[newArray.length - 1] = newContact;
+            contacts = newArray;
+            return true;
+        }
+    return false;
+    }
+
+    public static boolean isDuplicateMobileNumber (final String newContact){
+        // todo: implement the method
+        String[] newContactArray = newContact.split(","); //
+        String mobileNumber = newContactArray[1];
+        for (String contact : contacts) {
+            String indexMobilNumber = contact.split(",")[1];
+            if(indexMobilNumber.equals(mobileNumber)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int size(){
+        return contacts.length;
+    }
+
+    public static String[] getAllContacts(){
+        return  Arrays.copyOf(contacts,contacts.length);
+    }
+}
